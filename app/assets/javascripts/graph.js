4a4e84ac97d4a65a470b89ff58ba921f8efae3f7
william oleson <wnoleson@gmail.com>
2019-01-16 21:27:36 -0700

    fixing heroku bug

---
+++ app/assets/javascripts/graph.js
@@ -0,0 +1,25 @@
+
+// let myChart = document.getElementById('myChart').getContext('2d');
+
+//       let massPopChart = new Chart(myChart, {
+//         type:'bar', //types: bar, horizontal, pie, line, donut
+//         data: {
+//           labels:['Salt Lake City', 'Ogden', 'Farmington'],
+//           datasets: [{
+//             label:'Utah population',
+//             data: [
+//               186440,
+//               82825,
+//               18275
+//             ],
+//             backgroundColor: 'blue'
+//           }]
+//         },
+//         options:[]
+//       });
+
+
+// export {myChart}
+// export {massPopChart}
+
+// need to export/refactor in html
