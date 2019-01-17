6d9843a9d712b5071a37938d1ee2d3f8974cf0a5
william oleson <wnoleson@gmail.com>
2019-01-16 20:56:00 -0700

    test

---
+++ app/assets/javascripts/sidenav.js
@@ -0,0 +1,15 @@
+/* Set the width of the side navigation to 250px */
+// function openNav() {
+//   document.getElementById("mySidenav").style.width = "250px";
+// }
+
+//  Set the width of the side navigation to 0
+// function closeNav() {
+//   document.getElementById("mySidenav").style.width = "0";
+// }
+
+
+// export {openNav}
+// export {closeNav}
+
+//problems with import in the console.  William O
---
+++ db/migrate/20190116212221_add_user_to_pages.rb
@@ -0,0 +1,5 @@
+class AddUserToPages < ActiveRecord::Migration[5.2]
+  def change
+    add_column :pages, :name, :text
+  end
+end
